4e02c18b98a85192ba7715b2fdc85c3d930d25c3
Junhui <junhui@umich.edu>
2016-09-27 18:15:41 -0400

    feteching data from IMDB

--- si-601-hw-3/si-601-hw3_junhui.py
+++ si-601-hw-3/si-601-hw3_junhui.py
@@ -1,8 +1,29 @@
 from bs4 import BeautifulSoup
-import json, urllib2
+import json, urllib2,re
 
+
+####### Step 1
 response = urllib2.urlopen('http://www.imdb.com/search/title?at=0&sort=num_votes&count=100')
 initial_doc = response.read()
 soup = BeautifulSoup(initial_doc,'html.parser')
 
-print soup
+outputfile = open ("step1.html",'w')
+outputfile.write(soup.encode('utf-8'))
+outputfile.close()
+
+####### Step 2
+inputfile = open('step1.html','rU')
+outputfile = open('step2.txt','w')
+soup = BeautifulSoup(inputfile,'html.parser')
+
+outputfile.write("   ".join(['IMDB_ID','Rank','Title'])+'\n')
+
+IMDBdiv = soup.find_all('h3',class_='lister-item-header')
+IMDBlink = []
+for item in IMDBdiv:
+    link = item.find_all('a')
+    for a in link:
+        IMDBlink.append(a['href'])
+print IMDBlink
+for link in IMDBlink:
+    match = re.match(r'',link)
